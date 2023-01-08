8e47ddc09a2397d046be00825239cdedf5835f74
Vipul Patil <patilvipul701@gmail.com>
2023-01-09 03:04:28 +0530

    commit

--- BombaySoftwaresProject/settings.py
+++ BombaySoftwaresProject/settings.py
@@ -112,13 +112,25 @@ WSGI_APPLICATION = 'BombaySoftwaresProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+#PostgresSql Connection
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
