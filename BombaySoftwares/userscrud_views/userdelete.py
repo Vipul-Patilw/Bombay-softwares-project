ca7aafaed9a445c82edc1ba74ccfbc81ac41a90d
Vipul Patil <90356288+Vipul-Patilw@users.noreply.github.com>
2023-01-13 18:14:26 +0530

    Update userdelete.py

--- BombaySoftwares/userscrud_views/userdelete.py
+++ BombaySoftwares/userscrud_views/userdelete.py
@@ -2,6 +2,7 @@ from BombaySoftwares.models import UserRegistration
 from django.urls import reverse_lazy
 from django.views.generic.edit import DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 class UserAccountDelete(LoginRequiredMixin,DeleteView):
 		model = UserRegistration
 		template_name = 'delete_user_account.html'
